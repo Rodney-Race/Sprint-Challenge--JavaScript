da8ee6e0ef0d1ae98ea09f1f9827afa6b9a8814d
Rodney-Race <rodneyrace@gmail.com>
2019-08-16 23:34:41 -0400

    final update

--- challenges/classes.js
+++ challenges/classes.js
@@ -12,8 +12,13 @@ class CuboidMaker {
       return ( 2 * (this.length *this. width + this.length * this.height + this.width * this.height));
     }
 
-   }
+}
 // Test your volume and surfaceArea methods by uncommenting the logs below:
+const cubeeey = new CuboidMaker({
+    length: 4,
+    width: 5,
+    height: 5
+  })
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
 
